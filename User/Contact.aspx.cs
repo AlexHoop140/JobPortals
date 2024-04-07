3addb46a3205af7408de6fcb20988f1bca171331
AlexHoop140 <alexhoop.pro@gmail.com>
2024-04-08 00:28:06 +0700

    "add db connect"

--- User/Contact.aspx.cs
+++ User/Contact.aspx.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace JobPortals.User
 {
+    
     public partial class Contact : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
         {
 
         }
