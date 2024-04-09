307f7b1d4b6355b064a52ee5c5ef3359f97d7e37
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2024-04-09 14:31:32 +0700

    update - add sql queries for Part 2

--- DB_creation.sql
+++ DB_creation.sql
@@ -11,3 +11,50 @@ CREATE TABLE CONTACT(
 );
 
 SELECT * FROM CONTACT;
+
+USE JobPortal;
+
+CREATE TABLE Country(
+	CountryId INT PRIMARY KEY IDENTITY(1,1),
+	CountryName VARCHAR(50)
+)
+
+SELECT * FROM Country;
+
+INSERT INTO Country VALUES ('Vietnam');
+INSERT INTO Country VALUES 
+('France'),
+('Brazil'),
+('Canada'),
+('South Africa'),
+('Thailand'),
+('Russia'),
+('Mexico'),
+('Italy'),
+('Spain'),
+('Japan');
+
+CREATE TABLE [User](
+	UserId INT PRIMARY KEY IDENTITY(1,1),
+	Username VARCHAR(50),
+	Password VARCHAR(50),
+	Name VARCHAR(50),
+	Email VARCHAR(50),
+	Mobile VARCHAR(50),
+	TenthGrade VARCHAR(50),
+	TwelfthGrade VARCHAR(50),
+	GraduationGrade VARCHAR(50),
+	PostGraduationGrade VARCHAR(50),
+	Phd VARCHAR(50),
+	WorksOn VARCHAR(50),
+	Experience VARCHAR(50),
+	Resume VARCHAR(50),
+	Address VARCHAR(MAX),
+	Country VARCHAR(50)
+)
+
+ALTER TABLE [User] ADD UNIQUE (Username);
+
+SELECT * FROM [User];
+
+DELETE FROM [User];
