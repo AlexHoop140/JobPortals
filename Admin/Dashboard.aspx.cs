7d476333b185c2815185197c1ee58e96bd43a0fa
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2024-04-14 19:11:48 +0700

    part 11 - CK commited

--- Admin/Dashboard.aspx.cs
+++ Admin/Dashboard.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +9,97 @@ namespace JobPortals.Admin
 {
     public partial class Dashboard : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlDataAdapter sda;
+        DataTable dt;
+        string str = System.Configuration.ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("../User/Login.aspx");
+            }
+            
+            if (!IsPostBack)
+            {
+                Users();
+                Jobs();
+                AppliedJobs();
+                ContactCount();
+            }
+        }
 
+        private void ContactCount()
+        {
+            con = new SqlConnection(str);
+            sda = new SqlDataAdapter("SELECT COUNT(*) FROM Contact", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Session["Contact"] = dt.Rows[0][0];
+            }
+            else
+            {
+                Session["Contact"] = 0;
+
+            }
+        }
+
+        private void AppliedJobs()
+        {
+            con = new SqlConnection(str);
+            sda = new SqlDataAdapter("SELECT COUNT(*) FROM AppliedJobs", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Session["AppliedJobs"] = dt.Rows[0][0];
+            }
+            else
+            {
+                Session["AppliedJobs"] = 0;
+
+            }
+        }
+
+        private void Jobs()
+        {
+            con = new SqlConnection(str);
+            sda = new SqlDataAdapter("SELECT COUNT(*) FROM Jobs", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Session["Jobs"] = dt.Rows[0][0];
+            }
+            else
+            {
+                Session["Jobs"] = 0;
+
+            }
+        }
+
+        private void Users()
+        {
+            con = new SqlConnection(str);
+            sda = new SqlDataAdapter("SELECT COUNT(*) FROM [User]", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Session["Users"] = dt.Rows[0][0];
+            }
+            else
+            {
+                Session["User"] = 0;
+
+            }
         }
     }
 }
